f82c996163eb798bd703558d26fa53976442a9c5
诚勇刘 <liuchengyong@iluoteng.com>
2016-03-14 18:11:43 +0800

    [liu] 新增ios的plugin

---
+++ src/ios/CDVFolkMethd.h
@@ -0,0 +1,23 @@
+//
+//  CDVFolkMethd.h
+//  folk
+//
+//  Created by 孙虎林 on 16/3/14.
+//  Copyright © 2016年 luoteng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioServices.h>
+#import <Cordova/CDVPlugin.h>
+@interface CDVFolkMethd : CDVPlugin<UIAlertViewDelegate>
+
++(void)topicDetail;
+
++(void)articleId;
+
++(void)imageShow;
+
+
+
+@end
